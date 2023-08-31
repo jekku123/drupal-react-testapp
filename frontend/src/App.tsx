1b0190d9358d30c31c0460b57ffce60481b64bc0
Jesse Manninen <jesse.manninen@gmail.com>
2023-08-31 15:18:49 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    const [data, setData] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(
+                'http://localhost:56344/jsonapi/node/destination',
+                { mode: 'cors' }
+            );
+            const json = await response.json();
+            setData(json);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className='App'>
+            <h1>React + Drupal app</h1>
+            {data &&
+                data.data.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <h2>{item.attributes.title}</h2>
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: item.attributes.body.value,
+                                }}
+                            />
+                        </div>
+                    );
+                })}
+        </div>
+    );
+}
+
+export default App;
